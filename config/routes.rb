dde679caaa224576190a1e7a639f3a4ce46955ae
Jocelyn Naarden <tzionone@gmail.com>
2022-08-18 15:14:03 -0400

    Finished Assignment ✔

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# 
+resources :plants, only: [:index, :show, :create]
+#View all plants
+# get '/plants', to: 'plants#index'
+# #Show one plant
+# get '/plants/:id', to: 'plants#show'
+# #Create one plant
+# post '/plants', to: 'plants#create'
+
 end
